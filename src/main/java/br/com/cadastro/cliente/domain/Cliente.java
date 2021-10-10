8c0e2b619b1d640c99ecde814d7a17fa9e18e2d1
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-10 19:23:18 -0300

    Mapeando a classe Cliente para o banco de dados

---
+++ src/main/java/br/com/cadastro/cliente/domain/Cliente.java
@@ -0,0 +1,71 @@
+package br.com.cadastro.cliente.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 30)
+    private String nome;
+
+    @NotNull
+    @Size(min = 5, max = 30)
+    private String sobrenome;
+
+    @NotNull
+    @Size(min = 15, max = 100)
+    private String email;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(Long id, String nome, String sobrenome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
