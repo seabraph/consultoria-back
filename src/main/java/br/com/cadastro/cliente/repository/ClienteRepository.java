18f2cb30d5252800b6393e9848378fcd3cdfac1c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-10 19:41:01 -0300

    Adicionando annotation @Repository à interface ClienteRepository

--- src/main/java/br/com/cadastro/cliente/repository/ClienteRepository.java
+++ src/main/java/br/com/cadastro/cliente/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package br.com.cadastro.cliente.repository;
 
 import br.com.cadastro.cliente.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
