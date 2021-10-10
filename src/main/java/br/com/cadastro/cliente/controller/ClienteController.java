ec8faf7d747013bb33ee9796a2a0c92f0b5f8af8
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-10 19:48:48 -0300

    Criada o controller ClienteController

---
+++ src/main/java/br/com/cadastro/cliente/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package br.com.cadastro.cliente.controller;
+
+import br.com.cadastro.cliente.domain.Cliente;
+import br.com.cadastro.cliente.service.ClienteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+
+    // atributo do service
+    private ClienteService clienteService;
+
+    // método get para buscar todos os clientes
+    @GetMapping
+    public List<Cliente> getClientes() {
+        return clienteService.getCliente();
+    }
+}
