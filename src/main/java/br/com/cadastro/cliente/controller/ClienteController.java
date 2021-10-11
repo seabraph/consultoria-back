1d738d72d85ed45ac13904134ede98e1373eaf99
luantzc <luantzc@gmail.com>
2021-10-11 19:08:41 -0300

    ClienteController requisição post implementada

--- src/main/java/br/com/cadastro/cliente/controller/ClienteController.java
+++ src/main/java/br/com/cadastro/cliente/controller/ClienteController.java
@@ -3,9 +3,7 @@ package br.com.cadastro.cliente.controller;
 import br.com.cadastro.cliente.domain.Cliente;
 import br.com.cadastro.cliente.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,11 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> getClientes() {
         return clienteService.getClientes();
+    }
+
+    // método post para acrescentar novos clientes
+    @PostMapping
+    public void insertCliente(@RequestBody Cliente cliente) {
+        clienteService.insertCliente(cliente);
     }
 }
