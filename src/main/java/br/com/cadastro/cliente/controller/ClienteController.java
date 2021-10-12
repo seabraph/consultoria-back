556a0da3313080712c6d0c91a4e01686716ad0c2
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-12 02:05:54 -0300

    Removendo "/api" da url do ClienteController

--- src/main/java/br/com/cadastro/cliente/controller/ClienteController.java
+++ src/main/java/br/com/cadastro/cliente/controller/ClienteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cliente")
+@RequestMapping("/cliente")
 public class ClienteController {
 
     // atributo do service
